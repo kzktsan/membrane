363712424e49cf0f504a694caeaaf4e9e0357cd1
S.K <kzkt_rkrn@yahoo.co.jp>
2015-05-09 02:33:32 +0900

    fov_membraneファイルの追加

--- kirinuki.py
+++ kirinuki.py
@@ -20,7 +20,7 @@ for y in range(0, height - 1):
 		if (img[y, x, 2] > 200 and img[y, x, 1] < 100 and img[y, x, 0] <100):
 			if (y - half >= 0 and y + half < height and x - half >= 0 and x + half < width):
 				copy = img2[y - half : y + half +1 , x - half : x + half +1]
-				cv2.imwrite("/Users/satokazuki/Desktop/training_5/membrane" + str(count) + ".png", copy)
+				cv2.imwrite("/Users/satokazuki/Desktop/training_6/membrane_x" + str(x) + "y_"+str(y)+ ".png", copy)
 				count = count + 1
 			#f.write(str(x) + " " + str(y) + " " + str(img[y, x, 2]) + "\n")
 			
